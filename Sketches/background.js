60b402d4ed03d17c87c5df80d48e30d3a2a76229
Trek Hopton <trek.hopton@gmail.com>
2016-09-29 14:12:29 +0930

    tree version

--- Sketches/background.js
+++ Sketches/background.js
@@ -1,55 +1,61 @@
 var backSketch = function(b){
 
-  var circle1, circle2, circle3, circle4, circle5;
-
   b.setup = function() {
     var backCanvas = b.createCanvas(window.innerWidth, window.innerHeight);
-    b.fill(205,245, 255);
-    b.noStroke();
-    circle1 = new MCircle(b.random(b.height), b.random(), b.random());
-    circle2 = new MCircle(b.random(b.height), b.random(), b.random());
-    circle3 = new MCircle(b.random(b.height), b.random(), b.random());
-    circle4 = new MCircle(b.random(b.height), b.random(), b.random());
-    circle5 = new MCircle(b.random(b.height), b.random(), b.random());
+    b.translate(b.width/2, b.height/1.1);
+    b.strokeWeight(1);
+    b.frameRate(0.5);
   }
 
   b.draw = function() {
-    b.clear();
+    b.background(200);
+      tree(100);
+  }
 
-    var mouseX_ = b.mouseX + b.width/2;
-    var mouseY_ = b.mouseY + b.height/2;
+  function tree(trunkLength){
 
-    circle1.move(mouseX_-(b.width/2), mouseY_+(b.height*0.1), 30);
-    circle2.move((mouseX_+(b.width*0.05)), mouseY_+(b.height*0.025), 60);
-    circle3.move(mouseX_/5, mouseY_/2-(b.width*0.025), 40);
-    circle4.move(mouseX_-(b.width/2), (b.height-mouseY_), 50);
-    circle5.move(mouseX_/4,mouseY_/9-(b.height*0.035), 140);
-    circle1.display();
-    circle2.display();
-    circle3.display();
-    circle4.display();
-    circle5.display();
+    var baseAngle = b.randomGaussian(b.PI, b.PI/16);
+    // trunk
+    b.push();
+    b.rotate(baseAngle);
+    b.line(0,0,0,trunkLength);
+    b.translate(0,trunkLength);
+    branch(trunkLength, 0, 1);
+    b.pop();
+    // recursive branches
+    function branch(baseLength,segNo, gen){
+      // weighting of how often splits should happen
+      var splits = [1,1,1,2,2,2,2,3,3,4];
+      var split = splits[b.round(b.random(0,9))];
+      // base case
+      if(segNo >= 10 || split == 0 || gen >= 5){
+        return;
+      }
+      // branch continued
+      var length = baseLength*b.random(0.95, 0.99);
+      b.push();
+        b.rotate(b.randomGaussian(0,b.PI/12));
+        b.line(0,0,0,length);
+        b.translate(0,length);
+        segNo++;
+        branch(length*b.random(0.7, 0.9), segNo, gen);
+      b.pop();
+      // branche/s split off
+      for(var i = 0; i < split-1; i++){
+        length = baseLength*b.random(0.8, 0.9);
+        b.push();
+          b.rotate(b.random(-b.PI/3, b.PI/3));
+          b.line(0,0,0,length);
+          b.translate(0,length);
+          gen++;
+          branch(length*b.random(0.5, 0.9), 0, gen);
+        b.pop();
+      }
+    }
   }
 
   b.windowResized = function(){
     b.resizeCanvas(window.innerWidth, window.innerHeight);
-  }
-
-  function MCircle(iw, ixp, iyp) {
-    this.w = iw;
-    this.xpos = ixp*b.width;
-    this.ypos = iyp*b.height;
-
-    this.move = function(posX, posY, damping) {
-      var dif = this.ypos - posY;
-      this.ypos -= dif/damping;
-      dif = this.xpos - posX;
-      this.xpos -= dif/damping;
-    }
-
-    this.display = function() {
-        b.ellipse(this.xpos, this.ypos, this.w, this.w);
-    }
   }
 }
 var back_p5 = new p5(backSketch);
