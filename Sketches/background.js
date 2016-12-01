45f64318c17b5e6f5b412bb0d232d86e6e47410b
Trek Hopton <trek.hopton@gmail.com>
2016-12-01 16:52:11 +1030

    reflection and water working, not using colorLerp

--- Sketches/background.js
+++ Sketches/background.js
@@ -12,27 +12,44 @@ var backSketch = function(b){
 
   var bgColor;
 
+  var bgHue;
+  var bgSat;
+  var bgBright;
+
+  var wHue;
+  var wSat;
+  var wBright;
+
   var myWind = new wind();
 
   b.setup = function() {
     var backCanvas = b.createCanvas(window.innerWidth, window.innerHeight);
-    b.frameRate(24);
-    b.randomSeed(5249);
-    var bgTone = 200;
-    bgColor = b.color(bgTone);
+    b.frameRate(30);
+    b.randomSeed(55639);
+    b.colorMode(b.HSB);
+    //background color
+    bgHue = 180;
+    bgSat = 0;
+    bgBright = 90;
+    bgColor = b.color(bgHue, bgSat, bgBright);
+    //water color
+    wHue = 180;
+    wSat = 40;
+    wBright = 90;
+    wColor = b.color(wHue, wSat, wBright);
     //initialising trees
     var treeCount = 120;
     for(var i = 0; i < treeCount; i++){
-      var minDist = b.height*0.9;
+      var minDist = b.height*0.5;
       var maxDist = b.height*1.4;
       var iTreePosX = b.random(-b.width/10, b.width);
-      var iTreePosY = b.map(i, 0, treeCount, minDist, maxDist)-iTreePosX/3;
-      var lengthMin = 80;
+      var iTreePosY = b.map(i, 0, treeCount, minDist, maxDist)-iTreePosX/2;
+      var lengthMin = 20;
       var lengthMax = 200;
       var iTreeLength = b.random(lengthMin, lengthMax);
       var iTreeGirth = b.map(iTreeLength, lengthMin, lengthMax, 1, 8);
-      var iTreeColor = b.color(b.map(iTreePosY, minDist-iTreePosX/3, maxDist, bgTone, 0));
-      trees[i] = new tree(iTreePosX,iTreePosY, iTreeLength, iTreeGirth, iTreeColor);
+      var iTreeColor = b.color(0, 0, b.map(iTreePosY, minDist-iTreePosX/2, maxDist, bgBright, 0));
+      trees[i] = new tree(iTreePosX, iTreePosY, iTreeLength, iTreeGirth, iTreeColor);
     }
 
   }
@@ -41,38 +58,47 @@ var backSketch = function(b){
     console.log(b.frameRate());
     b.background(bgColor);
 
-    // wind
-    myWind.update();
+    for(var i = 0; i < trees.length; i++){
+      trees[i].displayReflection();
+    }
+    //water gradient
+    for(var i = 0; i < b.height; i++) {
+      var step = b.map(i, 0, b.height, 0, 1);
+      var iColor = b.color(b.lerp(bgHue, wHue, step), b.lerp(bgSat, wSat, step), b.lerp(bgBright, bgBright, step), 0.35);
+      b.stroke(iColor);
+      b.line(0, i, b.width, i);
+    }
 
     for(var i = 0; i < trees.length; i++){
-      // display each tree with the current wind
-      trees[i].display(myWind.wind);
+      trees[i].display();
     }
+
   }
 
   function tree(posx, posy, trunkLength, trunkGirth, color){
 
-    var baseAngle = b.random(b.PI, b.PI-b.PI/16);
+    var baseAngle = b.random(b.PI, b.PI+b.PI/16);
     var branchCont1 = new branch(trunkLength, trunkGirth, 0);
 
     // init the graphic for this tree
     var graphicWidth = 400;
-    var graphic = b.createGraphics(graphicWidth, 1000);
+    var graphicHeight = 1000;
+    var graphic = b.createGraphics(graphicWidth, graphicHeight);
 
     //move over a little on the graphic
-    graphic.translate(graphicWidth-trunkGirth, 0);
+    graphic.translate(trunkGirth, graphicHeight-trunkGirth);
     //set color
     graphic.stroke(color);
     // trunk
     graphic.push();
-      graphic.rotate(-baseAngle);
+      graphic.rotate(baseAngle);
       graphic.strokeWeight(trunkGirth);
-      graphic.line(0,0,0,-trunkLength);
-      graphic.translate(0,-trunkLength);
+      graphic.line(0,0,0,trunkLength);
+      graphic.translate(0,trunkLength);
       branchCont1.render();
     graphic.pop();
 
-    graphic.translate(-trunkGirth, 0);
+    graphic.translate(trunkGirth, 0);
 
     // recursive branches
     function branch(baseLength, baseGirth, segNo){
@@ -94,7 +120,7 @@ var backSketch = function(b){
       //branch continued
       var length = baseLength*b.random(0.95, 0.99);
       var girth = baseGirth*0.7;
-      var angle = b.random(-b.PI/16);
+      var angle = b.random(b.PI/16);
       var branchCont = new branch(length*b.random(0.7, 0.9), girth, segNo);
       // branch/es split off
       var branches = [];
@@ -102,7 +128,7 @@ var backSketch = function(b){
       var lengths = [];
       for(var i = 0; i < split-1; i++){
         lengths[i] = baseLength*b.random(0.4, 0.5);
-        angles[i] = b.random(0, -b.PI/3);
+        angles[i] = b.random(0, b.PI/3);
         branches.push(new branch(lengths[i]*b.random(0.5, 0.9), girth, segNo));
       }
 
@@ -110,19 +136,19 @@ var backSketch = function(b){
       this.render = function(){
         // branch continued
         graphic.push();
-          graphic.rotate(-angle);
+          graphic.rotate(angle);
           graphic.strokeWeight(girth);
-          graphic.line(0,0,0,-length);
-          graphic.translate(0,-length);
+          graphic.line(0,0,0,length);
+          graphic.translate(0,length);
           branchCont.render();
         graphic.pop();
         // branch/es split off
         for(var i = 0; i < split-1; i++){
           graphic.push();
-            graphic.rotate(-angles[i]);
+            graphic.rotate(angles[i]);
             graphic.strokeWeight(girth);
-            graphic.line(0,0,0,-lengths[i]);
-            graphic.translate(0,-lengths[i]);
+            graphic.line(0,0,0,lengths[i]);
+            graphic.translate(0,lengths[i]);
             branches[i].render();
           graphic.pop();
         }
@@ -130,11 +156,27 @@ var backSketch = function(b){
     }
     //branch end
 
-    this.display = function(currentWind){
+    this.display = function(){
+      // wind
+      myWind.update(posx);
+      //reeds
       b.push();
       b.translate(posx, posy);
-      b.rotate(b.PI+currentWind);
-      b.translate(-graphicWidth+trunkGirth, 0);
+      b.rotate(-myWind.wind);
+      b.translate(trunkGirth, -graphicHeight+trunkGirth);
+      b.image(graphic, 0, 0);
+      b.pop();
+    }
+
+    this.displayReflection = function(){
+      // wind
+      myWind.update(posx);
+      //reflection
+      b.push();
+      b.translate(posx, posy);
+      b.rotate(b.PI+myWind.wind);
+      b.translate(-trunkGirth, -graphicHeight+trunkGirth);
+      b.scale(-1, 1);
       b.image(graphic, 0, 0);
       b.pop();
     }
@@ -143,17 +185,16 @@ var backSketch = function(b){
   function wind(){
 
     var period = 0;
-    var amp = 0.02;
+    var phase = 0;
+    var amp = 0.04;
     this.wind;
 
-    this.update = function(){
-      period += b.PI/30;
-      this.wind = b.sin(period)*amp;
+    this.update = function(posx){
+      phase = posx/800;
+      period += b.PI/15000; // should make this loop
+      this.wind = b.sin(period-phase)*amp;
     }
-
-
   }
-
 
   b.windowResized = function(){
     b.resizeCanvas(window.innerWidth, window.innerHeight);
